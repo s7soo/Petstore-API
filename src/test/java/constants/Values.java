1096cce3b9164ec6850f20c8bf60834a6a00d549
Mohamed <=>
2025-02-18 20:01:26 +0200

    finished pet requests

--- src/test/java/constants/Values.java
+++ src/test/java/constants/Values.java
@@ -2,4 +2,6 @@ package constants;
 
 public class Values {
     public static String[] petStatus = {"available", "pending", "sold"};
+    public static String imgUrl = "src/main/resources/images/pet.png";
+    public static String apiKey = "12345678";
 }
--- src/test/java/tests/PetTest.java
+++ src/test/java/tests/PetTest.java
@@ -5,11 +5,12 @@ import io.restassured.response.Response;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.logging.Logger;
 
 
 import static constants.Urls.baseUrl;
-import static constants.Values.petStatus;
+import static constants.Values.*;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -33,6 +34,7 @@ public class PetTest {
         response.then().assertThat().statusCode(200);
         // verify body status value
         response.then().assertThat().body("[0].status", equalTo(status));
+        logger.info(String.valueOf(response.getStatusCode()));
     }
 
     @Test
@@ -44,6 +46,7 @@ public class PetTest {
         response.then().assertThat().statusCode(200);
         // verify body status value
         response.then().assertThat().body("[0].status", equalTo(status));
+        logger.info(String.valueOf(response.getStatusCode()));
     }
     @Test
     public void getSoldPets(){
@@ -54,10 +57,25 @@ public class PetTest {
         response.then().assertThat().statusCode(200);
         // verify body status value
         response.then().assertThat().body("[0].status", equalTo(status));
+        logger.info(String.valueOf(response.getStatusCode()));
+    }
+    @Test
+    public void getPetById(){
+        int id = 11616;
+        Response response =
+                given().
+                        header("accept", "application/json").
+                        header("api_key", apiKey).
+                        when().
+                        get("/pet/"+id).
+                        then().
+                        statusCode(200).
+                        extract().
+                        response();
     }
 
     @Test
-    public void postNewPet(){
+    public void createNewPet(){
         int id = 11616;
         String name = "CatFish";
         String photo = "";
@@ -79,6 +97,89 @@ public class PetTest {
                 body("status",equalTo(status)).
                 extract().
                 response();
+        logger.info(String.valueOf(response.getStatusCode()));
 
     }
+
+    @Test
+    public void updatePetPhoto(){
+        createNewPet();
+        int id = 11616;
+        File photo = new File(imgUrl);
+        Response response =
+                given().
+                        header("accept", "application/json").
+                        contentType("multipart/form-data").
+                        multiPart("file", photo, "image/png").
+                        when().
+                        post("/pet/"+id+"/uploadImage").
+                        then().
+                        statusCode(200).
+                        extract().
+                        response();
+        logger.info(String.valueOf(response.getStatusCode()));
+    }
+    @Test
+    public void updatePetDataUsingId(){
+        int id = 11616;
+        String name = "CatFishhh";
+        String status = petStatus[0];
+        Response response =
+                given().
+                        header("accept", "application/json").
+                        contentType("application/x-www-form-urlencoded").
+                        formParam("name",name).
+                        formParam("status",status).
+                        when().
+                        post("/pet/"+id).
+                        then().
+                        statusCode(200).
+                        body("message", equalTo(String.valueOf(id))).
+                        extract().response();
+        logger.info(String.valueOf(response.getStatusCode()));
+    }
+
+    @Test
+    public void updatePet(){
+        createNewPet();
+        int id = 11616;
+        String name = "CatFish";
+        String photo = "";
+        String status = petStatus[2];
+
+        Response response =
+                given().
+                        header("content-type","application/json").
+                        body(getRequestBody(
+                                id,
+                                name,
+                                photo,
+                                status
+                        )).
+                        when().
+                        put("/pet").
+                        then().
+                        statusCode(200).
+                        body("status",equalTo(petStatus[2])).
+                        extract().
+                        response();
+        logger.info(String.valueOf(response.getStatusCode()));
+    }
+
+    @Test
+    public void deletePet(){
+        int id = 11616;
+
+        Response response =
+                given().
+                        header("accept", "application/json").
+                        header("api_key", apiKey).
+                        when().
+                        delete("/pet/"+id).
+                        then().
+                        statusCode(200).
+                        extract().
+                        response();
+    }
+
 }
